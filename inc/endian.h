4d171b6d1900914459e7ff8ddfd9478e27bc7755
lizhiguo <zhiguoli@kunluntech.com.cn>
2025-01-07 17:33:06 +0800

    修复字节序转换编译问题

--- inc/endian.h
+++ inc/endian.h
@@ -9,12 +9,23 @@ static inline uint16_t htole16(uint16_t host_16bits)
 }
 
 uint16_t be16toh(uint16_t big_endian_16bits);
-uint16_t le16toh(uint16_t little_endian_16bits);
+static inline uint16_t le16toh(uint16_t little_endian_16bits)
+{
+    return little_endian_16bits;
+}
 
 uint32_t htobe32(uint32_t host_32bits);
-uint32_t htole32(uint32_t host_32bits);
+
+static inline uint32_t htole32(uint32_t host_32bits)
+{
+	return host_32bits;
+}
+
 uint32_t be32toh(uint32_t big_endian_32bits);
-uint32_t le32toh(uint32_t little_endian_32bits);
+static uint32_t le32toh(uint32_t little_endian_32bits)
+{
+    return little_endian_32bits;
+}
 
 uint64_t htobe64(uint64_t host_64bits);
 uint64_t htole64(uint64_t host_64bits);
